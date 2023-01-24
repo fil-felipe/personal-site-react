a0acee07ba80e16dedc0f804e0033f6083db2bd7
fil-felipe <filip.adamek@outlook.com>
2023-01-24 15:36:14 +0100

    content update

--- src/content.js
+++ src/content.js
@@ -58,8 +58,29 @@ const dataInput = [
                     ]
                 },
                 {
-                    blockTitle: "Test block single",
-                    blockContent: "test infor about such block"
+                    blockTitle: "Skills",
+                    blockContent: [
+                        {
+                            contentTitle: "Microsoft and Google Office sotware",
+                            contentString: "7 years work experience",
+                            contentArray: ["Microsoft Excel and Google spreadsheet. Advanced functions with VBA and AppScript", "Microsoft Word and Google Docs. Documentation with graphs and table of contents", "Microsoft Power Point and Google Presentation. Reports automaticaly linked and refreshed based on spreadsheet"]
+                        },
+                        {
+                            contentTitle: "SQL",
+                            contentString: "3 years work experience",
+                            contentArray: ["PostgreSQL and BigQuery experience", "Data extraction and creation mainly for reporting purposes", "Automatization of tasks based on procedures and other tools (for example AutomateNow)"]
+                        },
+                        {
+                            contentTitle: "Python",
+                            contentString: "2 years work experience + online practical courses on DataCamp and Udemy",
+                            contentArray: ["Reports creation", "Basic modeling and machine learning", "API creation based on Flask or Django", "Basic data enignering skills with creation of data pipeline.","Example of skills: http//www.google.pl Here will be Python GUI"]
+                        },
+                        {
+                            contentTitle: "Java script",
+                            contentString: "1 year experience + online practical course on Udemy",
+                            contentArray: ["Front end creation on React", "Backend creation based on express", "This website as a proof of knowledge", "Example of skills: http//www.google.pl Here will be Python GUI"]
+                        }
+                    ]
                 }
             ]
         }
@@ -118,6 +139,31 @@ const dataInput = [
                         {
                             contentTitle: "BMW financial Services",
                             contentString: "2014 - 2018"
+                        }
+                    ]
+                },
+                {
+                    blockTitle: "Umiejętności",
+                    blockContent: [
+                        {
+                            contentTitle: "Microsoft and Google Office sotware",
+                            contentString: "7 lat doświadczenia zawodowego",
+                            contentArray: ["Microsoft Excel, Google spreadsheet. Znajomość zaawansowanych funkcji + obsługa VBA (AppScript)", "Microsoft Word, Google Docs. Tworzenie dokumentacji z wykresami bazującymi na plikach oraz z aktywnym spisem treści", "Microsoft Power Point, Google Presentation. Tworzenie raportów z automatycznie odświeżanymi treściami na bazie innych plików (np. arkuszy danych)"]
+                        },
+                        {
+                            contentTitle: "SQL",
+                            contentString: "3 lata doświadczenia zawodowego + szkolenie na platformie Udemy",
+                            contentArray: ["Doświadczenie w PostgreSQL i BigQuery", "Wyciąganie danych z baz oraz zasianie baz danych głównie na potrzeby raportowania", "Automatyzacja procesu zasilania i przetwarzania danych za pomocą procedur w oparciu o inne narzędzia (np. AutomateNow)"]
+                        },
+                        {
+                            contentTitle: "Python",
+                            contentString: "2 lata doświadczenia zawodowego + szkolenia teoretyczne i praktyczne na platformie DataCamp oraz Udemy",
+                            contentArray: ["Tworzenie raportów wykorzystujących dynamiczne wykresy oraz bazujących na bazach danych (SQL, noSQL)", "Podstawowe tworzenie modeli oraz machine learning", "Tworzenie API przy użyciu pakietów Flask oraz Django", "Tworzenie podstawowych struktur danych i automatyczne zasianie baz.","Przykład umiejętności: http//www.google.pl tutaj będzie Python GUI"]
+                        },
+                        {
+                            contentTitle: "Java script",
+                            contentString: "1 rok doświadczenia + szkolenia teoretyczne i praktyczne na platformie Udemy",
+                            contentArray: ["Aplikacja webowa stwrzona w React", "Aplikacja Backend stworzona na bazie paczki express", "Ta strona została stworzona w React", "Inne przykłady: http//www.google.pl tutaj będzie GUI"]
                         }
                     ]
                 }
